9096912f6ccacb96eefc90fb5517a1ed597a460e
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-12 00:36:18 -0700

    Finalizacion del proyecto

--- app/src/main/java/com/example/pre_examen/ListActivity.java
+++ app/src/main/java/com/example/pre_examen/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.pre_examen;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import java.util.List;
 public class ListActivity extends AppCompatActivity {
     private ListView listViewUsuarios;
     private UsuariosDb usuariosDb;
+    private Button btnRegresarLst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,17 @@ public class ListActivity extends AppCompatActivity {
 
         listViewUsuarios = findViewById(R.id.listViewUsuarios);
         usuariosDb = new UsuariosDb(this);
+        btnRegresarLst = findViewById(R.id.btnRegresarLst);
 
 
         List<Usuario> listaUsuarios = usuariosDb.allUsuarios();
         UsuarioAdapter adapter = new UsuarioAdapter(this, listaUsuarios);
 
         listViewUsuarios.setAdapter(adapter);
+
+        btnRegresarLst.setOnClickListener(v -> {
+            finish();
+        });
     }
 
 
